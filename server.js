966da344baac95b2813877d2f06e9046b80b52ef
Alyssa Lundgren <lundgrea@gmail.com>
2019-09-30 18:32:25 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+import express from 'express';
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'BYOB';
+
+app.get('/', (request, response) => {
+  response.send('Oh hey there, BYOB');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
